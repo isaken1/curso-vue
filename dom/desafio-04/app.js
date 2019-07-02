427cce91dbb2b0ca682fff1739fa31accfb47fa7
Isaac Kennedy <isaackennedy@info.ufrn.br>
2019-07-02 17:13:10 -0300

    Fim do desafio 4

--- dom/desafio-04/app.js
+++ dom/desafio-04/app.js
@@ -1,11 +1,17 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		classe1: 'destaque',
+		custom1: 'c1',
+		custom2: 'c2',
+		classeExemplo: ''
 	},
 	methods: {
 		iniciarEfeito() {
-
+			setInterval(() => {
+				this.classe1 = this.classe1 == 'destaque' ?
+					'encolher' : 'destaque'
+			}, 1000)
 		},
 		iniciarProgresso() {
 
