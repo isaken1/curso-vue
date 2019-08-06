22608d4a120016f37fedc1b80e87f50011c0ddc4
Isaac Kennedy Diniz Gomes <isaac_kennedy@hotmail.com>
2019-08-06 17:44:43 -0300

    Novo módulo

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{titulo}}
+    <button @click="titulo += '#'"">Alterar</button>"
+  </div>
+</template>
+
+
+<script>
+export default {
+  data: function() {
+    return {
+      titulo: 'Teste Data usando Vue.js'
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+     background-color: chocolate;
+     color: white
+  }
+</style>
+
