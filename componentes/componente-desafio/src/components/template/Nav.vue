27dee8ce718503b56e896fad46f0845872422476
Isaac Kennedy Diniz Gomes <isaac_kennedy@hotmail.com>
2019-08-13 10:42:49 -0300

    Desafio Componentes

---
+++ componentes/componente-desafio/src/components/template/Nav.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-navigation-drawer app>
+        <toolbar-nav />
+
+        <v-divider></v-divider>
+
+        <v-list dense class="pt-0">
+            <v-list-tile v-for="item in itensMenu" :key="item.titulo">
+                <v-list-tile-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{ item.titulo }}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+import ToolbarNav from './ToolbarNav'
+export default {
+    components: {
+       'toolbar-nav': ToolbarNav
+    },
+    data() {
+        return {
+            itensMenu: [
+                {titulo: 'Inicio', icon: 'dashboard'},
+                {titulo: 'Sobre', icon: 'question_answer'}
+            ]
+        }
+    }
+}
+</script>
---
+++ componentes/componente-desafio/src/main/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-app>
+		<navbar />
+
+		<custom-toolbar />
+
+		<v-content>
+			<carousel />
+		</v-content>
+
+		<custom-footer />
+	</v-app>
+</template>
+
+<script>
+import CustomToolbar from '@/components/template/Toolbar'
+import Nav from '@/components/template/Nav'
+import Content from '@/components/widgets/Content'
+import Footer from '@/components/template/Footer'
+export default {
+	components: {
+		customToolbar: CustomToolbar,
+		navbar: Nav,
+		carousel: Content,
+		customFooter: Footer
+	}
+}
+</script>
