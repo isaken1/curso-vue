ccca55437f8ef217293b4071eba682ff257516ef
Isaac Kennedy <isaackennedy@info.ufrn.br>
2019-09-03 12:49:27 -0300

    Desafio de formulários

---
+++ formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="nome-completo">
+        <input type="text" @change="onChangeNome"
+            placeholder="Primeiro nome">
+        <input type="text" @change="onChangeSobrenome"
+            placeholder="Sobrenome">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        onChangeNome(event) {
+            this.value.nome = event.target.value
+            this.$emit('input', this.value)
+        },
+        onChangeSobrenome(event) {
+            this.value.sobrenome = event.target.value
+            this.$emit('input', this.value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+    input, textarea, select {
+        flex: 1;
+        font-size: 1.4rem;
+        outline: none;
+        border: 1px solid #DDD;
+        border-radius: 5px;
+        padding: 5px 10px;
+        margin-right: 15px;
+        align-self: stretch;
+    }
+
+
+    input:focus {
+        border: 1px solid #BBB;
+    }
+
+    .nome-completo {
+        display: flex;
+
+        margin: 15px 0px;
+        font-size: 1.4rem;
+    }
+
+</style>
--- formularios/formulario-exercicios/src/App.vue
+++ formularios/formulario-exercicios/src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<h1>Registrar Reclamação</h1>
 		<div class="conteudo">
-			<form class="painel">
+			<form class="painel" v-if="!enviado">
 				<div class="cabecalho">Formulário</div>
 				<Rotulo nome="E-mail">
 					<input type="text" v-model.lazy.trim="usuario.email">
@@ -34,12 +34,12 @@
 					</select>
 				</Rotulo>
 				<Rotulo nome="Primeira Reclamação?">
-					<Escolha />
+					<Escolha v-model="escolha" />
 				</Rotulo>
 				<hr>
-				<button>Enviar</button>
+				<button @click.prevent="enviar">Enviar</button>
 			</form>
-			<div class="painel">
+			<div class="painel" v-else>
 				<div class="cabecalho">Resultado</div>
 				<Rotulo nome="E-mail">
 					<span>{{ usuario.email }}</span>
@@ -67,7 +67,7 @@
 					<span>{{ prioridade }}</span>
 				</Rotulo>
 				<Rotulo nome="Primeira Reclamação?">
-					<span>???</span>
+					<span>{{ escolha }}</span>
 				</Rotulo>
 			</div>
 		</div>
@@ -96,7 +96,14 @@ export default {
 				email: '',
 				senha: '',
 				idade: 25
-			}
+			},
+			escolha: true,
+			enviado: false
+		}
+	},
+	methods: {
+		enviar() {
+			this.enviado = true
 		}
 	}
 }
